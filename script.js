685695551981c2cbe28cc239fec4cf15903e9865
Samuel Osa-Agbontaen <sosaagbontaen@college.harvard.edu>
2024-03-31 04:14:54 -0400

    Updated education section and projects section

--- script.js
+++ script.js
@@ -1,38 +1,38 @@
 const tabs = document.querySelectorAll(".tabs-toggle");
 
-function urlUpdate(pageNameItem, urlItem){
-    if(pageNameItem == "welcome"){
+function urlUpdate(pageNameItem, urlItem) {
+    if (pageNameItem == "welcome") {
         urlItem.textContent = "Welcome to my digital portfolio!";
     }
-    else if (pageNameItem == "education"){
-        urlItem.textContent = "Relevant coursework towards my A.B in Computer Science."
+    else if (pageNameItem == "education") {
+        urlItem.textContent = "Relevant coursework"
     }
-    else if(pageNameItem == "skills"){
+    else if (pageNameItem == "skills") {
         urlItem.textContent = "Here are some skills I've picked up so far.";
     }
-    else if(pageNameItem == "experience"){
+    else if (pageNameItem == "experience") {
         urlItem.textContent = "I've had exciting opportunities to learn and work beyond the classroom.";
     }
-    else if(pageNameItem == "projects"){
+    else if (pageNameItem == "projects") {
         urlItem.textContent = "I like showcasing what I've learned through projects. Check them out!";
     }
-    else if(pageNameItem == "contact"){
+    else if (pageNameItem == "contact") {
         urlItem.textContent = "I'm looking forward to hearing from you!";
-    }        
+    }
 }
 
-function tabLogic(){
+function tabLogic() {
     for (let i = 0; i < tabs.length; i++) {
-        tabs[i].addEventListener('click', function() {
-            
+        tabs[i].addEventListener('click', function () {
+
             //Find current active tab & page
             let cur_tab = document.querySelector('.activeTab');
             let cur_page = document.querySelector('.activePage');
-            
+
             //De-activate active tab & page
             cur_tab.className = cur_tab.className.replace(' activeTab', '');
             cur_page.className = cur_page.className.replace(' activePage', '');
-            
+
             //Activate clicked tab
             this.className += ' activeTab';
 
@@ -40,49 +40,49 @@ function tabLogic(){
             //console.log(pageNamefromTab);
             //console.log(document.querySelector(".page-wrapper." + pageNamefromTab));
             let newPage = document.querySelector(".page-wrapper." + pageNamefromTab);
-            
+
             //Activate new page
             newPage.className += ' activePage';
 
             //Update URL Bar
             let urlBar = document.querySelector("#url-bar");
-            urlUpdate(pageNamefromTab,urlBar);
+            urlUpdate(pageNamefromTab, urlBar);
         })
-        
+
     }
 }
 
-function classListReplace(elt, checkfor, replacewith){
-    if (elt.classList.contains(checkfor)){
+function classListReplace(elt, checkfor, replacewith) {
+    if (elt.classList.contains(checkfor)) {
         elt.classList.add(replacewith);
         elt.classList.remove(checkfor);
     }
-    else{
+    else {
         elt.classList.add(checkfor);
         elt.classList.remove(replacewith);
     }
 }
 
-function toggleTheme(){
+function toggleTheme() {
     let toggler = document.querySelector("#theme-toggle")
     let affected_items = document.querySelectorAll(".day");
-    toggler.addEventListener('click', function(){
+    toggler.addEventListener('click', function () {
         affected_items.forEach(affected_item => {
-        classListReplace(affected_item,"night","day");
+            classListReplace(affected_item, "night", "day");
         });
     })
 }
 
-function mobileTabLogic(){
+function mobileTabLogic() {
     const hamburger = document.querySelector(".tab-hamburger");
     const statusbar = document.querySelector(".statusbar-wrapper");
-    hamburger.addEventListener('click', function(){
-        if (statusbar.classList.contains('open')){
-            classListReplace(statusbar, 'open','closed');
+    hamburger.addEventListener('click', function () {
+        if (statusbar.classList.contains('open')) {
+            classListReplace(statusbar, 'open', 'closed');
             statusbar.style.height = "60px";
         }
-        else{
-            classListReplace(statusbar, 'closed','open');
+        else {
+            classListReplace(statusbar, 'closed', 'open');
             statusbar.style.height = "180px";
         }
     })
